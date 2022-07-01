d76823178f8e385f04e7c5c2b14a6264e1cd3496
Jake Edgar <jakeedgar1012@gmail.com>
2022-07-01 10:35:53 -0700

    add change handlers

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -17,13 +17,75 @@ class KegControl extends React.Component {
     }
   }
 
+  handleClick = () => {
+    if (this.state.selectedKeg != null) {
+      this.setState({
+        formVisibleOnPage: false,
+        selectedKeg: null,
+        editing: false
+      });
+    } else {
+      this.setState(prevState => ({
+        formVisibleOnPage: !prevState.formVisibleOnPage,
+      }));
+    }
+  }
+
+  handleAddingNewKegToList = (newKeg) => {
+    const newMainKegList = this.state.mainKegList.concat(newKeg);
+    this.setState({
+      mainKegList: newMainKegList,
+      formVisibleOnPage: false
+    });
+  }
+
+  handleChangingSelectedKeg = (id) => {
+    const selectedKeg = this.state.mainKegList.filter(keg => keg.id === id)[0];
+    this.setState({selectedKeg: selectedKeg});
+  }
+
+  handleDeletingKeg = (id) => {
+  const newMainKegList = this.state.mainKegList.filter(keg => keg.id !== id);
+  this.setState({
+    mainKegList: newMainKegList,
+    selectedKeg: null
+  });
+}
+
+  handleEditClick = () => {
+    this.setState({editing: true});
+  }
+
+  handleEditingKegInList = (kegToEdit) => {
+  const editedMainKegList = this.state.mainKegList
+    .filter(keg => keg.id !== this.state.selectedKeg.id)
+    .concat(kegToEdit);
+  this.setState({
+      mainKegList: editedMainKegList,
+      editing: false,
+      selectedKeg: null
+    });
+}
+
   render() {
     
     let currentlyVisibleState = null;
+    let buttonText = null;
+
+    if(this.state.editing) {
+      currentlyVisibleState = null;
+    }
+
+    currentlyVisibleState = 
+      <KegList 
+        mainKegList={this.state.mainKegList} 
+        onKegSelection={this.handleChangingSelectedKeg} 
+        buttonText = "Add Keg"/>;
 
     return(
       <React.Fragment>
         <div>
+          {currentlyVisibleState}
         </div>
         <button>Submit</button>
       </React.Fragment>
@@ -32,11 +94,3 @@ class KegControl extends React.Component {
 }
 
 export default KegControl;
-
-
-    // id: PropTypes.string,
-    // name: PropTypes.string,
-    // content: PropTypes.number,
-    // price: PropTypes.number,
-    // pints: PropTypes.number,
-    // whenKegClicked: PropTypes.func
