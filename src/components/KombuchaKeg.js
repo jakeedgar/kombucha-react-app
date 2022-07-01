aa6a2857d921520f2c2e0406d74a638c0502824c
Jake Edgar <jakeedgar1012@gmail.com>
2022-07-01 13:22:10 -0700

    update styling with custom card bodies rows and columns

--- src/components/KombuchaKeg.js
+++ src/components/KombuchaKeg.js
@@ -3,12 +3,18 @@ import PropTypes from "prop-types"
 
 function KombuchaKeg (props){
   return(
-    <div>
-      <div onClick= {() => props.whenKegClicked(props.id)}>
-        <h1>{props.name} - costs {props.price}$</h1>
-        <h3>{props.content} % Alcohol Per Pint</h3>
-        <h3>{props.pints} Pints Remaining</h3>
-        <h3>{props.pintsSold} Pints Sold So Far</h3>
+    <div className="container">
+      <div className="row">
+        <div className="column">
+          <div className="card">
+            <div onClick= {() => props.whenKegClicked(props.id)}>
+              <h1>{props.name} - costs {props.price}$</h1>
+              <h3>{props.content} % Alcohol Per Pint</h3>
+              <h3>{props.pints} Pints Remaining</h3>
+              <h3>{props.pintsSold} Pints Sold So Far</h3>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   )
